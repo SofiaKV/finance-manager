d202e64e3eedea831c27cca870bee489e891861f
SofiaKV <162361442+SofiaKV@users.noreply.github.com>
2025-10-07 15:17:00 +0300

    Set up React frontend

---
+++ frontend/jest.config.ts
@@ -0,0 +1,9 @@
+﻿const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['./jest/setupTests.ts'],
+  transform: { '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.app.json' }] },
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+};
+
+export default config;
---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          Vite Docs
+        </a>{' '}
+        |{' '}
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          React Docs
+        </a>
+        Personal Finance
+      </p>
+    </>
+  );
+}
+
+export default App;
